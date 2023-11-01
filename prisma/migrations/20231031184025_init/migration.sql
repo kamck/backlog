556dc96cea105a506d576fd0b4e48c5a2a78a85d
kamck <kamck@users.noreply.github.com>
2023-11-01 11:25:16 -0500

    Add new migration

---
+++ prisma/migrations/20231031184025_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Playthrough" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "platform" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "startedOn" DATETIME,
+    "finishedOn" DATETIME,
+    "completeTime" INTEGER,
+    "notes" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
